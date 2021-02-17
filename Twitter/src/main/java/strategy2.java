6720d5d1ce42b55a558cbc7966a5809307119205
radoslavovm <radoslavov.m@northeastern.edu>
2021-02-17 18:49:35 -0500

    strat2 test1

--- Twitter/src/main/java/strategy2.java
+++ Twitter/src/main/java/strategy2.java
@@ -2,7 +2,11 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.ScanParams;
 import redis.clients.jedis.ScanResult;
 import java.util.List;
-
+/*
+    This is the Broadcasting Strategy. As tweets are inserted, the timelines of users are populated with tweet ids
+    The users and their tweet timelines are then printed.
+    The user.csv is user id & id of person that follows said user
+ */
 public class strategy2 implements TweetDbAPI {
     public Jedis jedis = new Jedis("localhost");
 
@@ -25,15 +29,32 @@ public class strategy2 implements TweetDbAPI {
     @Override
     public void insert_tweet(String tweet_text, int user_id) {
         // key tweet:id val user:id
-        // for this tweet , loop through list of people that follow user , call home_screen to add tweet to follower
+        jedis.set("Tweet_id/User:"+ jedis.get("next_tweet_id"), String.valueOf(user_id));
         // key tweet:id val tweet:text
+        jedis.set("Tweet_id/Text:"+ jedis.get("next_tweet_id"), tweet_text);
+        // key user:id val list of tweet:id the user is author of
+        jedis.lpush("User_id:"+user_id, jedis.get("next_tweet_id"));
+
+        String cur = ScanParams.SCAN_POINTER_START;
+        List<String> followers = null;
+        //all the followers that need to have timelines populated
+        ScanResult<String> scanFollowers = jedis.sscan("User:"+ user_id, cur);
+        followers = scanFollowers.getResult();
+
+        for (int u=0; u < followers.size(); u++) {
+            home_screen(user_id, Integer.parseInt(followers.get(u)));
+        }
+        jedis.incrBy("next_tweet_id", 1);
     }
 
     @Override
-    public void home_screen(int user_id) {
+    public void home_screen(int user_id, int f) {
+        List<String> tweets = jedis.lrange("User_id:"+user_id, 0, -1);
 
-        // key user , list tweet ids
-        // system out user
-        //system out tweets
+        for (int t=0; t < tweets.size(); t++) {
+            jedis.lpush("TimeLine:"+f , tweets.get(t));
+        }
+
+        // system out user author, tweet
     }
 }
